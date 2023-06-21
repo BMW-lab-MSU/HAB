36f80364af3b87d114caa94379a9ef2f3207ecff
wyattzyatt <wyattzyatt@gmail.com>
2023-06-21 14:34:07 -0600

    Update npy_to_mat.py
    corrected order of steps

--- npy_to_mat.py
+++ npy_to_mat.py
@@ -16,8 +16,8 @@ def npy_to_mat
     for image_file in tqdm(os.listdir(dir),position=0,leave=True):
         print("\n")
         try:
+            img_fileName = image_file[:-4]+".mat"
             file_path = mat_dir+"/"+img_fileName
-            img_fileName = image_file[:-4]+".png"
             image_np = np.load(dir+"/"+image_file)
             scipy.io.savemat(file_path, {img_fileName, image_np})
 
