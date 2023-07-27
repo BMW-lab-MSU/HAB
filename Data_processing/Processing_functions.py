502aa50a6324ac54f9946bf36b40fb1546bfd635
Flint Morgan <FlintMorgan32@gmail.com>
2023-07-27 15:38:24 -0600

    used pcolormesh to increase speed

--- Data_processing/Processing_functions.py
+++ Data_processing/Processing_functions.py
@@ -24,7 +24,9 @@ def raw_to_stokes_mono(raw):
     S2 = I45 - I135
     print("S2:",np.min(S2),np.max(S2))
     DoLP = np.sqrt((S1**2)+(S2**2))/S0
+    print("DoLP:",np.min(DoLP),np.max(DoLP))
     AoP =0.5*np.arctan2(S2,S1)* 180/np.pi
+    print("AoP:",np.min(AoP),np.max(AoP))
     
     return(I90, I45, I0, I135, S0, S1, S2, DoLP, AoP)
 
@@ -43,21 +45,21 @@ def false_color(DIR,frame):
                     I90, I45, I0, I135, S0, S1, S2, DoLP, AoP = raw_to_stokes_mono(image_np)
                     
                     plt.subplot(2,3,4)
-                    plt.pcolor(DoLP,cmap=plt.cm.turbo,vmin=0, vmax=1)
+                    plt.pcolormesh(DoLP,cmap=plt.cm.turbo,vmin=0, vmax=1)
                     plt.gca().invert_yaxis()
                     plt.colorbar()
                     plt.title(image_file[-9:-4].replace("n"," n")+" DoLP Frame:"+str(frame))
                     plt.tight_layout()
                     
                     plt.subplot(2,3,5)
-                    plt.pcolor(AoP,cmap=plt.cm.hsv,vmin=-90, vmax=90)
+                    plt.pcolormesh(AoP,cmap=plt.cm.hsv,vmin=-90, vmax=90)
                     plt.gca().invert_yaxis()
                     plt.colorbar()
                     plt.title(image_file[-9:-4].replace("n"," n")+" AoP Frame:"+str(frame))
                     plt.tight_layout()
                     
                     plt.subplot(2,3,1)
-                    plt.pcolor(S0,vmin=0, vmax=2*np.mean(S0))
+                    plt.pcolormesh(S0,vmin=0, vmax=2*np.mean(S0))
                     plt.colorbar()
                     plt.gca().invert_yaxis()
                     plt.title(image_file[-9:-4].replace("n"," n")+" S0 Frame:"+str(frame))
@@ -65,7 +67,7 @@ def false_color(DIR,frame):
 
                     
                     plt.subplot(2,3,2)
-                    plt.pcolor(S1,vmin=0, vmax=2*np.mean(S1))
+                    plt.pcolormesh(S1,vmin=0, vmax=2*np.mean(S1))
                     plt.colorbar()
                     plt.gca().invert_yaxis()
                     plt.title(image_file[-9:-4].replace("n"," n")+" S1 Frame:"+str(frame))
@@ -73,26 +75,26 @@ def false_color(DIR,frame):
 
                     
                     plt.subplot(2,3,3)
-                    plt.pcolor(S2,vmin = 2*np.mean(S2), vmax=0)
+                    plt.pcolormesh(S2,vmin = 2*np.mean(S2), vmax=0)
                     plt.colorbar()
                     plt.gca().invert_yaxis()
                     plt.title(image_file[-9:-4].replace("n"," n")+ " S2 Frame:"+str(frame))
                     plt.tight_layout()
 
-                    
+                    """
                     c_over_a = p[3]*(DoLP**3)+p[2]*(DoLP**2)+p[1]*(DoLP)+p[0]
                     print("c/a:",np.min(c_over_a),np.max(c_over_a))
                     print("mean:",np.mean(c_over_a))
                     plt.subplot(2,3,6)
                     if 2*np.mean(c_over_a)<0:
-                        plt.pcolor(c_over_a,vmin=2*np.mean(c_over_a),vmax=10)
+                        plt.pcolormesh(c_over_a,vmin=2*np.mean(c_over_a),vmax=10)
                     else:
-                        plt.pcolor(c_over_a,vmax=2*np.mean(c_over_a),vmin=-10)
+                        plt.pcolormesh(c_over_a,vmax=2*np.mean(c_over_a),vmin=-10)
                     plt.colorbar()
                     plt.gca().invert_yaxis()
                     plt.title(image_file[-9:-4].replace("n"," n")+ " c/a Frame:"+str(frame))
                     plt.tight_layout()
-                    
+                    """
                     plt.show()
                     
                     
